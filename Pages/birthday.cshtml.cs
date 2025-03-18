cc46c0aa87c0b431defb77163fa80925c6d8f1b8
LunaBroeke <onefivefoursakura@gmail.com>
2025-03-18 01:08:53 +0100

    create page

---
+++ Pages/birthday.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LunaSite.Pages
+{
+    public class birthdayModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
